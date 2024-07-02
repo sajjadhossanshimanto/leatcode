c928d9949a1b67531d08e08cb743a8c862f8a17a
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-02 21:59:34 +0600

    initil code

---
+++ 4. network-delay-time.py
@@ -0,0 +1,39 @@
+'''
+https://leetcode.com/problems/network-delay-time/
+'''
+#%%
+from typing import List
+from collections import defaultdict
+from heapq import heappush, heappop
+
+
+inf = float("inf")
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = defaultdict(list)
+
+        for u, v, w in times:
+            adj[u].append((v, w))
+        
+        node = k
+        # def dijksta(node):
+        sssp = [inf]*(n+1)
+        sssp[node] = 0
+
+        max_dis = 0
+        visit = set()
+
+        pq = []
+        heappush(pq, (node, 0))
+        while pq:
+            node, dis = heappop(pq)
+            visit.add(node)
+            if dis>sssp[node]: continue
+            for child, w in adj[node]:
+                if w+dis>sssp[child]: continue
+                sssp[child] = w+dis
+                heappush(pq, (child, w+dis))
+                max_dis = max(max_dis, w+dis)
+        if len(visit)!=n: return -1
+        return max_dis
