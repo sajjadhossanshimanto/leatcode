d4c786ae97a6f6a1b1c629a0f6263f9b1d293027
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-03 16:04:28 +0600

    wa bug fixes

--- sortest_path/3. find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ sortest_path/3. find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -9,6 +9,8 @@ from collections import deque
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
         def bfs(node, money):
+            visit = set()
+            visit.add(node)
             q = deque()
             q.append((node, money))
             c = 0
@@ -16,7 +18,9 @@ class Solution:
                 node, money = q.popleft()
                 for child, w in adj[node]:
                     if child in visit: continue
-                    if w>money: return c
+                    visit.add(node)
+                    if w>money: 
+                        continue
                     c+=1
                     if money==w: continue# no more traversal needed
                     q.append((child, money-w))
@@ -27,10 +31,9 @@ class Solution:
             adj[a].append((b, w))
             adj[b].append((a, w))
 
-        min_nei = 0
+        min_nei = float('inf')
         min_node = -1
         for i in range(n):
-            visit = set()
             neiber = bfs(i, distanceThreshold)
             if neiber==min_nei:
                 if i>min_node: min_node = i
@@ -41,5 +44,9 @@ class Solution:
         return min_node
 #%%
 Solution().findTheCity(n = 4, edges = [[0,1,3],[1,2,1],[1,3,4],[2,3,1]], distanceThreshold = 4)
-
+# ans = 3
+# %%
+Solution().findTheCity(n = 5, edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]], distanceThreshold = 2)
+# ex = 0
+# out = 4
 # %%
