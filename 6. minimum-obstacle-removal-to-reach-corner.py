ef6cffc28035cf2d7a947d804d785252c6a25e2d
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-04 14:16:57 +0600

    faulty dijkstra

--- 6. minimum-obstacle-removal-to-reach-corner.py
+++ 6. minimum-obstacle-removal-to-reach-corner.py
@@ -7,6 +7,7 @@ https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/descripti
 from typing import List
 from collections import defaultdict, deque
 from functools import lru_cache
+from heapq import heappop, heappush
 
 
 move = (
@@ -20,67 +21,40 @@ class Solution:
     def valid(self, x, y):
         return self.x>x>=0 and 0<y<self.y
 
-    # @lru_cache
-    def dfs(self, x, y):# use dfs to update bfs  value
+    def dijkstra(self, x, y):
+        pq = []
+        heappush(pq, (0, x, y))
+        sssp = [[inf]*self.y for _ in range(self.x)]
 
-        # if (x, y) == (self.x-1, self.y-1):
-        #     self.min_cost = min(cost, self.min_cost)
-        #     return True
+        visit = set((0, 0))
 
-        # if self.grid[x][y]:
-        #     cost+= 1
-        # else:
-        self.visit[x][y] = 1
-
-        for adx, ady in move:
-            cx = x+adx
-            cy = y+ady
-            if not self.valid(cx, cy): continue
-            if self.visit[cx][cy] or self.grid[cx][cy]: continue
-
-            self.dfs(cx, cy)
-
-        return x, y# TODO: is it guranted x y is valid and not a brick
-
-    def bfs(x, y):
-        q = deque()
-        q.append((x, y, cost))
-        self.level[x][y] = 0
-
-        while q:
-            x, y, cost = q.popleft()
+        while pq:
+            dis, x, y = heappop(pq)
+            if (x, y) == (self.x-1, self.y-1):
+                return dis
 
             for adx, ady in move:
                 cx = x+adx
                 cy = y+ady
-                # in bfs thers no way going backword
-                # so no need of visit
                 if not self.valid(cx, cy): continue
                 
-                child_cost = cost
-                if self.grid[cx]c[y]:
-                    child_cost+=1
+                if (cx, cy) in visit: continue
+                visit.add((cx, cy))
+                
+                nd = dis
+                if self.grid[cx][cy]:
+                    nd+=1
 
-                # if self.level[x][y] == -1:
-                self.level[cx][cy] = child_cost
-                q.append((cx, cy, child_cost))
-                # else:# not going to happen
-                #     child_cost<
+                if nd>sssp[cx][cy]: continue
+                sssp[cx][cy] = nd
+                heappush(pq, (nd, cx, cy))
 
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         self.grid = grid
         self.x = len(grid)# row
         self.y = len(grid[0])# col
 
-        # self.debug=[]
-        # self.debug.append((cx, cy))
-        # self.debug.pop()
-        # self.visit = [[0]*self.y for _ in range(self.x)]
-        self.min_cost = inf
-        self.level = [[-1]*self.y for _ in range(self.x)]
-
-        self.dfs(0, 0, 0)
-        return self.min_cost
+        return self.dijkstra(0, 0)
 
 g = Solution()
 #%%
