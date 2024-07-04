d7ea24a04f730e43050892fd8ccde3c5911d2970
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-04 20:06:48 +0600

    removing self. saves 50ms

--- 6. minimum-obstacle-removal-to-reach-corner.py
+++ 6. minimum-obstacle-removal-to-reach-corner.py
@@ -5,8 +5,6 @@ https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/descripti
 '''
 #%%
 from typing import List
-from collections import defaultdict, deque
-from functools import lru_cache
 from heapq import heappop, heappush
 
 
@@ -18,48 +16,50 @@ move = (
 )
 inf = float("inf")
 class Solution:
-    def valid(self, x, y):
-        return self.x>x>=0 and 0<=y<self.y
-
-    def dijkstra(self, x, y):
-        pq = []
-        heappush(pq, (0, x, y))
-        sssp = [[inf]*self.y for _ in range(self.x)]
-
-        visit = set((0, 0))
-
-        while pq:
-            dis, x, y = heappop(pq)
-            if (x, y) == (self.x-1, self.y-1):
-                return dis
-
-            for adx, ady in move:
-                cx = x+adx
-                cy = y+ady
-                if not self.valid(cx, cy): continue
-                
-                if (cx, cy) in visit: continue
-                visit.add((cx, cy))
-                
-                nd = dis
-                if self.grid[cx][cy]:
-                    nd+=1
-
-                if nd>sssp[cx][cy]: continue
-                sssp[cx][cy] = nd
-                heappush(pq, (nd, cx, cy))
-
     def minimumObstacles(self, grid: List[List[int]]) -> int:
-        self.grid = grid
-        self.x = len(grid)# row
-        self.y = len(grid[0])# col
+        
+        row = len(grid)# row
+        col = len(grid[0])# col
 
-        return self.dijkstra(0, 0)
+        def valid(x, y):
+            return row>x>=0 and 0<=y<col
+        
+        def dijkstra(x, y):
+            pq = []
+            heappush(pq, (0, x, y))
+            sssp = [[inf]*col for _ in range(row)]
+
+            visit = set((0, 0))
+
+            while pq:
+                dis, x, y = heappop(pq)
+                if (x, y) == (row-1, col-1):
+                    return dis
+
+                for adx, ady in move:
+                    cx = x+adx
+                    cy = y+ady
+                    if not valid(cx, cy): continue
+                    
+                    if (cx, cy) in visit: continue
+                    visit.add((cx, cy))
+                    
+                    nd = dis
+                    if grid[cx][cy]:
+                        nd+=1
+
+                    if nd>sssp[cx][cy]: continue
+                    sssp[cx][cy] = nd
+                    heappush(pq, (nd, cx, cy))
+
+        return dijkstra(0, 0)
 
 g = Solution()
 #%%
+# ans = 2
 g.minimumObstacles(grid = [[0,1,1],[1,1,0],[1,1,0]])
 # %%
+# ans = 0
 g.minimumObstacles(grid = [[0,1,0,0,0],[0,1,0,1,0],[0,0,0,1,0]])
 # %%
 class Solution:
