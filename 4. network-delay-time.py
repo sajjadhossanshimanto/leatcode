9355178a738d613cd294106dde7804b9239083ad
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-04 09:58:38 +0600

    if in visitresolved tl anc gets ac

--- 4. network-delay-time.py
+++ 4. network-delay-time.py
@@ -22,13 +22,17 @@ class Solution:
         sssp[node] = 0
 
         visit = set()
+        max_time = node# never possible as sssp[node] is set to zero
 
         pq = []
         heappush(pq, (0, node))
         while pq:
             dis, node = heappop(pq)
+            if dis>sssp[node]or node in visit: continue
             visit.add(node)
-            if dis>sssp[node]: continue
+
+            if sssp[node]>sssp[max_time]: 
+                max_time = node
 
             for child, w in adj[node]:
                 nd = w+dis
@@ -36,19 +40,29 @@ class Solution:
                 sssp[child] = nd
                 heappush(pq, (nd, child))
 
-        if len(visit)!=n: return -1
-        return max(sssp[1:])
+        if len(visit)!=n or max_time==k: return -1
+        return sssp[max_time]
 
 #%%
 Solution().networkDelayTime(times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2)
+# ex = 2
 # %%
 Solution().networkDelayTime(times = [[1,2,1]], n = 2, k = 1)
+# ans = 1
 # %%
 Solution().networkDelayTime(times = [[1,2,1]], n = 2, k = 2)
+# ans = -1
 #%%
 Solution().networkDelayTime([[1,2,1],[2,3,2],[1,3,4]], n=3, k=1)
 # ex = 3
-# out = 4
+# out = 4--resolved
+#%% time limit
+from test_helper import sample_test
+
+sample_test(Solution().networkDelayTime, "39tl", r"testcase\4.json")
+# use 
+# bfs to levelel graph
+# dfs to go to the firthest
 #%%
 Solution().networkDelayTime
 Solution().networkDelayTime
