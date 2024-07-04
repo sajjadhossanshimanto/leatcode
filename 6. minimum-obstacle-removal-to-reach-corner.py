a0dd9c7105a2c156c3280df05e2178a0547d5ae6
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-04 13:06:04 +0600

    dfs failed

--- 6. minimum-obstacle-removal-to-reach-corner.py
+++ 6. minimum-obstacle-removal-to-reach-corner.py
@@ -17,16 +17,17 @@ move = (
 inf = float("inf")
 class Solution:
     def valid(self, x, y):
-        return self.x<x>=0 and 0<y<self.y
+        return self.x>x>=0 and 0<y<self.y
 
     def dfs(self, x, y, cost):
         if (x, y) == (self.x-1, self.y-1):
             self.min_cost = min(cost, self.min_cost)
-            return
-        
-        self.visit[x][y] = 1
+            return True
+
         if self.grid[x][y]:
             cost+= 1
+        else:
+            self.visit[x][y] = 1
 
         for adx, ady in move:
             cx = x+adx
@@ -34,8 +35,10 @@ class Solution:
             if not self.valid(cx, cy): continue
             if self.visit[cx][cy]: continue
 
+            self.debug.append((cx, cy))
             self.dfs(cx, cy, cost)
-        
+            self.debug.pop()
+
         # return x, y# TODO: is it guranted x y is valid and not a brick
 
     def minimumObstacles(self, grid: List[List[int]]) -> int:
@@ -43,12 +46,16 @@ class Solution:
         self.x = len(grid)# row
         self.y = len(grid[0])# col
 
+        self.debug=[]
         self.visit = [[0]*self.y for _ in range(self.x)]
         self.min_cost = inf
 
         self.dfs(0, 0, 0)
         return self.min_cost
 
+g = Solution()
 #%%
-Solution().minimumObstacles(grid = [[0,1,1],[1,1,0],[1,1,0]])
+g.minimumObstacles(grid = [[0,1,1],[1,1,0],[1,1,0]])
+# %%
+g.minimumObstacles(grid = [[0,1,0,0,0],[0,1,0,1,0],[0,0,0,1,0]])
 # %%
