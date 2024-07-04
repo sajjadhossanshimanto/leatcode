8253fb2617a1aedf9c7c5c5826f4aff90710bc9b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-04 13:49:42 +0600

    failed bfs. need modified dijkstra

--- 6. minimum-obstacle-removal-to-reach-corner.py
+++ 6. minimum-obstacle-removal-to-reach-corner.py
@@ -5,7 +5,8 @@ https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/descripti
 '''
 #%%
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, deque
+from functools import lru_cache
 
 
 move = (
@@ -19,36 +20,64 @@ class Solution:
     def valid(self, x, y):
         return self.x>x>=0 and 0<y<self.y
 
-    def dfs(self, x, y, cost):
-        if (x, y) == (self.x-1, self.y-1):
-            self.min_cost = min(cost, self.min_cost)
-            return True
+    # @lru_cache
+    def dfs(self, x, y):# use dfs to update bfs  value
 
-        if self.grid[x][y]:
-            cost+= 1
-        else:
-            self.visit[x][y] = 1
+        # if (x, y) == (self.x-1, self.y-1):
+        #     self.min_cost = min(cost, self.min_cost)
+        #     return True
+
+        # if self.grid[x][y]:
+        #     cost+= 1
+        # else:
+        self.visit[x][y] = 1
 
         for adx, ady in move:
             cx = x+adx
             cy = y+ady
             if not self.valid(cx, cy): continue
-            if self.visit[cx][cy]: continue
+            if self.visit[cx][cy] or self.grid[cx][cy]: continue
 
-            self.debug.append((cx, cy))
-            self.dfs(cx, cy, cost)
-            self.debug.pop()
+            self.dfs(cx, cy)
 
-        # return x, y# TODO: is it guranted x y is valid and not a brick
+        return x, y# TODO: is it guranted x y is valid and not a brick
+
+    def bfs(x, y):
+        q = deque()
+        q.append((x, y, cost))
+        self.level[x][y] = 0
+
+        while q:
+            x, y, cost = q.popleft()
+
+            for adx, ady in move:
+                cx = x+adx
+                cy = y+ady
+                # in bfs thers no way going backword
+                # so no need of visit
+                if not self.valid(cx, cy): continue
+                
+                child_cost = cost
+                if self.grid[cx]c[y]:
+                    child_cost+=1
+
+                # if self.level[x][y] == -1:
+                self.level[cx][cy] = child_cost
+                q.append((cx, cy, child_cost))
+                # else:# not going to happen
+                #     child_cost<
 
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         self.grid = grid
         self.x = len(grid)# row
         self.y = len(grid[0])# col
 
-        self.debug=[]
-        self.visit = [[0]*self.y for _ in range(self.x)]
+        # self.debug=[]
+        # self.debug.append((cx, cy))
+        # self.debug.pop()
+        # self.visit = [[0]*self.y for _ in range(self.x)]
         self.min_cost = inf
+        self.level = [[-1]*self.y for _ in range(self.x)]
 
         self.dfs(0, 0, 0)
         return self.min_cost
