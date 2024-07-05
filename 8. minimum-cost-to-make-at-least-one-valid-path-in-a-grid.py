cf72cf10ff4e87ffe0d35d5d834058cd96302338
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-05 11:28:18 +0600

    best solution with manual deque

--- 8. minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ 8. minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -61,3 +61,41 @@ g.minCost(grid = [[1,1,3],[3,2,2],[1,1,4]])
 # ans = 1
 g.minCost([[1,2],[4,3]])
 # %%
+# approach --> manual deque with 2 list
+
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        queue = [(m-1,n-1)]
+        potential = []
+        cost = 0
+        while True:
+            for x,y in queue:
+                if x == 0 and y == 0:
+                    return cost
+                if grid[x][y] > 0:
+                    grid[x][y] = 0
+                    if x > 0:
+                        if grid[x-1][y] == 3:
+                            queue.append((x-1,y))
+                        elif grid[x-1][y] > 0:
+                            potential.append((x-1,y))
+                    if x < m-1:
+                        if grid[x+1][y] == 4:
+                            queue.append((x+1,y))
+                        elif grid[x+1][y] > 0:
+                            potential.append((x+1,y))
+                    if y > 0:
+                        if grid[x][y-1] == 1:
+                            queue.append((x,y-1))
+                        elif grid[x][y-1] > 0:
+                            potential.append((x,y-1))
+                    if y < n-1:
+                        if grid[x][y+1] == 2:
+                            queue.append((x,y+1))
+                        elif grid[x][y+1] > 0:
+                            potential.append((x,y+1))
+            cost += 1
+            queue = potential
+            potential = []
