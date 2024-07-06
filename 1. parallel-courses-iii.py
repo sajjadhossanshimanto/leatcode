131ae51d44159b6331e9bd704cfbc5fef87f8ed6
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-06 21:42:03 +0600

    given test case

--- 1. parallel-courses-iii.py
+++ 1. parallel-courses-iii.py
@@ -72,6 +72,8 @@ g=Solution()
 #%%
 g.minimumTime(n = 3, relations = [[1,3],[2,3]], time = [3,2,5])
 # %%
+# ans = 12
+# out = 9-----fixed
 g.minimumTime(n = 5, relations = [[1,5],[2,5],[3,5],[3,4],[4,5]], time = [1,2,3,4,5])
 #%%
 g.root
