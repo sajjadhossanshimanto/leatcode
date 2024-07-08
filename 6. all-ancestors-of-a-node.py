736d0f579edca7ce1d88fea1353451c581f68d8c
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-08 20:07:07 +0600

    logic error fix

--- 6. all-ancestors-of-a-node.py
+++ 6. all-ancestors-of-a-node.py
@@ -4,7 +4,7 @@ https://leetcode.com/problems/all-ancestors-of-a-node-in-a-directed-acyclic-grap
 #%%
 from typing import List
 from collections import defaultdict
-from itertools import chain
+# from itertools import chain
 
 
 class Solution:
@@ -24,7 +24,9 @@ class Solution:
                     r = ans[child]
                 else:
                     r = dfs(child)
-                ansistor = chain(ansistor, r)
+                # r = r.copy()
+                # ansistor = chain(ansistor, r)
+                ansistor = ansistor+[child]+r
             
             ans[node] = sorted(ansistor)
             # print(node, ans[node])
@@ -40,5 +42,6 @@ class Solution:
 
 g = Solution()
 #%%
+# ans = [[],[],[],[0,1],[0,2],[0,1,3],[0,1,2,3,4],[0,1,2,3]]
 g.getAncestors(n = 8, edges = [[0,3],[0,4],[1,3],[2,4],[2,7],[3,5],[3,6],[3,7],[4,6]])
 # %%
