7370acc6863e4396583bfab46e1f69251d57331a
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-08 14:21:24 +0600

    optimization with ac

--- 4.find safe states.py
+++ 4.find safe states.py
@@ -3,6 +3,7 @@ https://leetcode.com/problems/find-eventual-safe-states/description/
 '''
 #%%
 from typing import List
+# from heapq import heappop, heappush
 
 
 class Solution:
@@ -12,9 +13,6 @@ class Solution:
         def dfs(node):
             # print(node)
             visit.add(node)
-            # if not graph[node]:
-            #     ans.append(node)
-            #     return True
 
             safe = True
             for child in graph[node]:
@@ -22,19 +20,22 @@ class Solution:
                     safe = False
                     continue
                 
-                if child in ans: continue
+                if child in done: continue
                 if not dfs(child):
                     safe = False
             
             if safe:
+                # heappush(ans, node)
                 ans.append(node)
-                # print('append', node)
+                print('append', node)
                 visit.remove(node)
+                done.add(node)
                 return True
 
         ans = []
+        done = set()
         for i in range(len(graph)):
-            if i in visit or i in ans: continue
+            if i in visit or i in done: continue
             dfs(i)
         
         ans.sort()
@@ -44,4 +45,8 @@ g = Solution()
 #%%
 #  ans = [2, 4, 5, 6]
 g.eventualSafeNodes([[1,2],[2,3],[5],[0],[5],[],[]])
+# %% tl111
+from test_helper import sample_test
+
+sample_test(g.eventualSafeNodes, 'tl111', r"testcase\4.json")
 # %%
