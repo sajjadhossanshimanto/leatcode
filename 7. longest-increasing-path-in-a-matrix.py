9f98781f4004b4ba83dc5c94ada4a3583326d11e
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-09 12:26:48 +0600

    seen solution

--- 7. longest-increasing-path-in-a-matrix.py
+++ 7. longest-increasing-path-in-a-matrix.py
@@ -1,16 +1,9 @@
 '''
 https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+sol -> https://www.youtube.com/watch?v=wCc_nd-GiEc
 '''
 #%%
 from typing import List
-from collections import defaultdict
-from operator import gt, lt
-
-
-def print_grid(matrix):
-    # chould have use pandas dataframe print function
-    print(*matrix, sep="\n")
-    print()
 
 
 move = (
@@ -30,48 +23,31 @@ class Solution:
         def isvalid(x, y):
             return 0<=x<gx and 0<=y<gy
 
-        # visit = [[0]*gy for _ in range(gx)]
-        visit = defaultdict(lambda :defaultdict(lambda :0))
+        dp = {}# he don't uses such double defaultdict
         def dfs(x, y):
-            visit[x][y]=1
-            # print(matrix[x][y])
-            # print_grid(visit)
+            if (x, y) in dp:
+                return dp[(x, y)]
 
-            ans = 1# this node itself
+            ans = 1
             for adx, ady in move:
                 cx, cy = (x+adx, y+ady)
                 if not isvalid(cx, cy): continue
-                if visit[cx][cy]: continue
+                
+                if matrix[cx][cy]>matrix[x][y]:
+                    ans = max(ans, dfs(cx, cy)+1)
             
-                if op(matrix[x][y], matrix[cx][cy]):
-                    # child+=1# for this child itself
-                    ans=max(dfs(cx, cy)+1, ans)
-        
-            # print(f"{matrix[x][y]} ans->", ans)
+            dp[(x, y)] = ans
             return ans
-
-        op = None
-        ans = 0
+        
         for x in range(gx):
             for y in range(gy):
-                # if visit[x][y]: continue
+                if dp.get((x, y)): continue
 
-                for adx, ady in move:
-                    cx, cy = (x+adx, y+ady)
-                    if not isvalid(cx, cy): continue
-                    # if visit[cx][cy]: continue
-                    if matrix[x][y] == matrix[cx][cy]: continue
-
-                    if matrix[x][y] > matrix[cx][cy]:
-                        op = gt
-                    elif matrix[x][y] < matrix[cx][cy]:
-                        op = lt
-                    # print(op)
-                    visit.clear()
-                    r = dfs(x, y)
-                    ans = max(ans, r)
+                dfs(x, y)
         
-        return ans
+        return max(dp.values())# he sas its the same as tracking max with avariable
+
+
 
 g = Solution()
 #%%
@@ -90,6 +66,10 @@ g.longestIncreasingPath([[1,2],[2,3]])
 # ans = 6
 # out = 4
 g.longestIncreasingPath([[7,8,9],[9,7,6],[7,2,3]])
+#%% wa128
+# ans = 8
+# out = 7
+g.longestIncreasingPath([[19,2,8,6,4,14,1,0,17],[0,1,9,10,11,4,12,14,5],[14,12,16,0,15,8,5,2,8],[5,4,1,17,9,18,8,5,2],[9,5,4,8,16,7,11,5,0],[5,7,14,18,10,0,14,14,0],[9,14,4,13,18,16,9,12,10],[18,13,9,18,11,4,12,10,10],[7,14,16,19,10,19,11,6,4],[16,2,3,7,15,9,7,1,1],[1,6,16,15,18,6,6,1,14],[9,5,2,9,8,3,2,3,10],[2,3,16,8,7,7,0,18,16],[11,0,16,8,13,13,11,3,8],[17,11,0,12,11,15,12,17,0]])
 #%%
 g.longestIncreasingPath
 g.longestIncreasingPath
