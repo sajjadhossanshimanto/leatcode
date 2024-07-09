91674297bedc1df065e27cfe07fb965a11ae8335
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-09 20:02:39 +0600

    this approach not gona word. by multiple iteration word may becomes linkable to a non adjecent world

--- 3. word ladder.py
+++ 3. word ladder.py
@@ -17,12 +17,19 @@ class Solution:
             for j in range(len(beginWord)):
                 if beginWord[j]!=word[j]:
                     c+=1
+            
             if c==1:
-                return end-i
+                print(end, i)
+                return end-i+1# 1 for the 1st conversion from start word to word on list
 
 g = Solution()
 #%%
+# ans = 5
 g.ladderLength(beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"])
 # %%
+# ans = 0
 g.ladderLength(beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"])
+# %% wa2
+# ans = 2
+g.ladderLength("a", "c", ["a", "b", "c"])
 # %%
