d5207441f4c132855e79f8a26deb34171517895f
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-11 13:20:43 +0600

    initial code with ac

---
+++ 10. surrounded regions.py
@@ -0,0 +1,76 @@
+#%%
+from typing import List
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        gx, gy = len(board), len(board[0])
+
+        visit = set()
+        def dfs(x, y):
+            visit.add((x, y))
+
+            for cx, cy in (
+                (x+1, y),
+                (x-1, y),
+                (x, y+1),
+                (x, y-1)
+            ):
+                if 0<=cx<gx and 0<=cy<gy and board[cx][cy]=='O' and (cx, cy) not in visit:
+                    dfs(cx, cy)
+
+        def in_place(x, y):
+            board[x][y] = "X"
+            
+            for cx, cy in (
+                (x+1, y),
+                (x-1, y),
+                (x, y+1),
+                (x, y-1)
+            ):
+                # if 0<=cx<gx and 0<=cy<gy:
+                #     (cx, cy) not in visit:
+                if board[cx][cy]!='X':
+                    in_place(cx, cy)
+
+        #up
+        x= 0
+        for y in range(gy):
+            if board[x][y]=='O':
+                dfs(x, y)
+        # down
+        x= gx-1
+        for y in range(gy):
+            if board[x][y]=='O':
+                dfs(x, y)
+        # left
+        y= 0
+        for x in range(gx):
+            if board[x][y]=='O':
+                dfs(x, y)
+        # right
+        y= gy-1
+        for x in range(gx):
+            if board[x][y]=='O':
+                dfs(x, y)
+
+        for x in range(gx):
+            for y in range(gy):
+                if board[x][y]=='O' and (x, y) not in visit:
+                    in_place(x, y)
+
+g = Solution()
+#%%
+inp = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+g.solve(inp)
+print(inp)
+inp ==  [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+
+# %%
+inp = [["X"]]
+g.solve(inp)
+print(inp)
+# %%
