417b0c3c843915921eeaba2945b522eb3cbd02bf
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-12 19:02:19 +0600

    initial faulty code

---
+++ 14.rotten iranges.py
@@ -0,0 +1,57 @@
+'''
+https://leetcode.com/problems/rotting-oranges/description/
+'''
+#%%
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        gx, gy = len(grid), len(grid[0])
+
+        def bfs(x, y):
+            q = deque()
+            q.append((x, y, 0))
+            # level = 0
+            ans = 0
+            while q:
+                x, y, level = q.popleft()
+                grid[x][y] = 0
+                ans = max(ans, level)
+
+                for cx, cy in (
+                    (x+1, y),
+                    (x-1, y),
+                    (x, y+1),
+                    (x, y-1)
+                ):
+                    if cx<0 or cy<0 or cx==gx or cy==gy:
+                        continue
+
+                    if grid[x][y]==2:
+                    # what if thers 2 rotten in same active 
+                        level = 0
+                        ans = ans//2
+
+                    if grid[x][y]==1:
+                        q.append((cx, cy, level+1))
+            
+            return ans
+
+        ans = 0
+        for x in range(gx):
+            for y in range(gy):
+                if grid[x][y]==2:
+                    ans = max(bfs(x, y), ans)
+
+        for x in range(gx):
+            if 1 in grid[x]: return -1
+        
+        return ans
+
+
+g = Solution()
+#%%
+g.orangesRotting([[2,1,1],[1,1,0],[0,1,1]])
+# %%
