5c4ddceb061f0351fd2e4a391e54b2c5966cbfc5
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-14 13:55:21 +0600

    need dijkstra. simole bfs fowing the sorter edge will alwaay get trapped

--- 12. swim in rising water.py
+++ 12. swim in rising water.py
@@ -1,7 +1,9 @@
-#%%
-from collections import deque
+'''
+https://leetcode.com/problems/swim-in-rising-water/description/
+'''
+#%%deque
 from typing import List
-from heapq import heappop, heappush
+from collections import deque
 
 
 inf = float("inf")
@@ -9,34 +11,29 @@ class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         gx, gy = len(grid), len(grid[0])
 
-        q=[]
-        q.append((grid[0][0], 0, 0, -inf))
+        visit = [[0]*gy for _ in range(gx)]
+        q = deque()
+        ans = [inf]
+        def dfs(x, y):
+            visit[x][y] = 1
+            if (cx, cy) == (gx-1, gy-1):# reached end 
+                return min(grid[x][y], ans[0])
 
-        visit = set()
-        sssp = {}
-        ans = inf
-        while q:
-            dis, x, y, max_ = heappop(q)
-            # print(grid[x][y], "-->", dis)
-
+            min_value = inf
+            min_node = None
             for cx, cy in (
                 (x+1, y),
                 (x-1, y),
                 (x, y+1),
                 (x, y-1)
             ):
-                if (cx, cy) == (gx-1, gy-1):# reached end 
-                    ans = min(max_, grid[cx][cy], ans)
+                if cx<0 or cy<0 or cx==gx or cy==gy or visit[cx][cy]:
                     continue
-
-                if cx<0 or cy<0 or cx==gx or cy==gy or (cx, cy) in visit:
-                    continue
-                visit.add((cx, cy))
-
-                nd = dis+grid[cx][cy]
-                if nd<=sssp.get((cx, cy), inf):
-                    sssp[(cx, cy)] = nd
-                    heappush(q, (nd, cx, cy, max(max_, grid[cx][cy])))
+                
+                if grid[cx][cy]<min_value: # whta if equal
+                    min_node = (cx, cy)
+            
+            dfs(*min_node)
 
         return ans
 
@@ -57,4 +54,3 @@ g.swimInWater(l)
 # out = 15
 g.swimInWater([[10,12,4,6],[9,11,3,5],[1,7,13,8],[2,0,15,14]])
 
-# %%
