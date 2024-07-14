1de80fe641181041f4cf274e1f0b0e119c985a19
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-14 14:09:09 +0600

    modified dijkstra. maximum value in sortest path

--- 12. swim in rising water.py
+++ 12. swim in rising water.py
@@ -4,23 +4,23 @@ https://leetcode.com/problems/swim-in-rising-water/description/
 #%%deque
 from typing import List
 from collections import deque
+from heapq import heappush, heappop
 
 
 inf = float("inf")
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         gx, gy = len(grid), len(grid[0])
-
         visit = [[0]*gy for _ in range(gx)]
-        q = deque()
-        ans = [inf]
-        def dfs(x, y):
-            visit[x][y] = 1
-            if (cx, cy) == (gx-1, gy-1):# reached end 
-                return min(grid[x][y], ans[0])
 
-            min_value = inf
-            min_node = None
+        q = []
+        heappush(q, (-inf, 0, 0))
+        visit[0][0] = 1# cummon midtaked by me. often forgets to add inital node to visit
+        while q:
+            mx_elevation, x, y = heappop(q)
+            if (x, y) == (gx-1, gy-1):# reached end 
+                return mx_elevation
+
             for cx, cy in (
                 (x+1, y),
                 (x-1, y),
@@ -29,13 +29,11 @@ class Solution:
             ):
                 if cx<0 or cy<0 or cx==gx or cy==gy or visit[cx][cy]:
                     continue
-                
-                if grid[cx][cy]<min_value: # whta if equal
-                    min_node = (cx, cy)
-            
-            dfs(*min_node)
+                visit[x][y] = 1
 
-        return ans
+                heappush(q, (max(mx_elevation, grid[cx][cy]), cx, cy))
+
+        return -1
 
 g=Solution()
 #%%
