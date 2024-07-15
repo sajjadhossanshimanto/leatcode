98cd7362d0def4187cf8969e7840b21b758c957b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-15 15:12:20 +0600

    redusing calls beats 93

--- 17. shortest bridge.py
+++ 17. shortest bridge.py
@@ -9,8 +9,6 @@ from collections import deque
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         gx, gy = len(grid), len(grid[0])
-        def isValid(x, y):
-            return 0<=x<gx and 0<=y<gy
 
         visit = [[0]*gy for _ in range(gx)]
         source = set()
@@ -23,38 +21,35 @@ class Solution:
                 (x, y+1),
                 (x, y-1)
             ):
-                if isValid(cx, cy):
+                if 0<=cx<gx and 0<=cy<gy:
                     if grid[cx][cy]==0:
                         source.add((x, y))
                     elif not visit[cx][cy]:
                         dfs(cx, cy)
 
-        def bfs():
-            q = deque(source)
-            res = 0
-            while q:
-                for _ in range(len(q)):
-                    x, y = q.popleft()
-                    for cx, cy in (
-                        (x+1, y),
-                        (x-1, y),
-                        (x, y+1),
-                        (x, y-1)
-                    ):
-                        if isValid(cx, cy) and visit[cx][cy]==0:
-                            if grid[cx][cy]:
-                                return res
-                            
-                            visit[cx][cy] = 1
-                            q.append((cx, cy))
-                res+=1
-
         for x in range(gx):
             for y in range(gy):
                 if grid[x][y]:
                     dfs(x, y)
-                    # return source
-                    return bfs()
+                    
+                    q = deque(source)
+                    res = 0
+                    while q:
+                        for _ in range(len(q)):
+                            x, y = q.popleft()
+                            for cx, cy in (
+                                (x+1, y),
+                                (x-1, y),
+                                (x, y+1),
+                                (x, y-1)
+                            ):
+                                if 0<=cx<gx and 0<=cy<gy and visit[cx][cy]==0:
+                                    if grid[cx][cy]:
+                                        return res
+                                    
+                                    visit[cx][cy] = 1
+                                    q.append((cx, cy))
+                        res+=1
 
 
 g = Solution()
