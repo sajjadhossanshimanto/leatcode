a48467cc3a59ca8a94a4b87beb732dade93a96db
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-15 13:09:25 +0600

    graph constraction

---
+++ 16.snakes & ladders.py
@@ -0,0 +1,38 @@
+'''
+https://leetcode.com/problems/snakes-and-ladders/description/
+- this question really clears idea witch one and why should a proablem be graph proablem
+- jesob question e akta pos thake. abong ak position thake onno position e jete hoy
+- hote pare conditional hote pare unconditionally
+- egulai graph proablem
+'''
+#%%
+from typing import List
+from collections import deque, defaultdict
+
+
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        # gx, gy = len(board), len(board[0])
+        n = len(board)+1# squire board and 1 indexed
+        cell = n*n
+
+        maps = {}
+        num=1
+        for x in range(gx):
+            for y in range(gy):
+                if board[x][y]!=-1:
+                    maps[num] = board[x][y]
+                    num+=1
+        # num = 1
+        visit = [0]*(cell+1)
+        adj = [[] for _ in range(cell+1)]
+        for i in range(1, cell+1):
+            for j in range(i+1, i+7):
+                if not visit[j]:
+                    visit[j] = 1
+                    if j in maps:
+                        adj[i].append(maps[j])
+                    else:
+                        adj[i].append(j)
+
+#%%
