94d520e7768889e81de3fb609ae98ddf4994a1e9
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-15 14:53:11 +0600

    ac beats 33

---
+++ 17. shortest bridge.py
@@ -0,0 +1,68 @@
+'''
+https://leetcode.com/problems/shortest-bridge/description/
+'''
+#%%
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        gx, gy = len(grid), len(grid[0])
+        def isValid(x, y):
+            return 0<=x<gx and 0<=y<gy
+
+        visit = set()
+        def dfs(x, y):
+            visit.add((x, y))
+
+            for cx, cy in (
+                (x+1, y),
+                (x-1, y),
+                (x, y+1),
+                (x, y-1)
+            ):
+                if isValid(cx, cy) and grid[cx][cy] and (cx, cy) not in visit:
+                    dfs(cx, cy)
+        
+        def bfs():
+            # use visit list as source list
+            # no matter we are looping over some extra nodes
+            # but thats saves memory
+            q = deque(visit)
+            res = 0
+            while q:
+                for _ in range(len(q)):
+                    x, y = q.popleft()
+                    for cx, cy in (
+                        (x+1, y),
+                        (x-1, y),
+                        (x, y+1),
+                        (x, y-1)
+                    ):
+                        if isValid(cx, cy) and (cx, cy) not in visit:
+                            if  grid[cx][cy]:
+                                return res
+                            
+                            visit.add((cx, cy))
+                            q.append((cx, cy))
+                res+=1
+
+        for x in range(gx):
+            for y in range(gy):
+                if grid[x][y]:
+                    dfs(x, y)
+                    return bfs()
+
+
+g = Solution()
+# %%
+# ans = 1
+g.shortestBridge([[0,1],[1,0]])
+# %%
+ans = 2
+g.shortestBridge([[0,1,0],[0,0,0],[0,0,1]])
+#%%
+ans = 1
+g.shortestBridge([[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]])
+# %%
