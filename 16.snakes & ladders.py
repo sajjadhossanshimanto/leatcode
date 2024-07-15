d599f62b6eda78789bb66fe40fe44535801f421a
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-15 14:08:31 +0600

    ac

--- 16.snakes & ladders.py
+++ 16.snakes & ladders.py
@@ -24,24 +24,24 @@ class Solution:
             return x, y
 
         # bfs
-        visit = [0]*(cell+1)
+        visit = set()
         q = deque()
         q.append((1, 0))# pos, moves used to reach that node
         while q:
             pos, move = q.popleft()
 
             for nxt_pos in range(pos+1, pos+7):
-                if visit[nxt_pos]: continue
-
                 x, y = num_to_pos(nxt_pos)
                 if board[x][y]!=-1:
                     nxt_pos = board[x][y]
 
                 if nxt_pos==cell:
                     return move+1
+                
                 # avoidend getting visit
-                visit[nxt_pos] = 1
-                q.append((nxt_pos, move+1))
+                if nxt_pos not in visit:
+                    visit.add(nxt_pos)
+                    q.append((nxt_pos, move+1))
 
         return -1
 
@@ -49,4 +49,6 @@ g = Solution()
 #%%
 # ans = 4
 g.snakesAndLadders( [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]])
+# %% ml6
+g.snakesAndLadders([[1,1,-1],[1,1,1],[-1,1,1]])
 # %%
