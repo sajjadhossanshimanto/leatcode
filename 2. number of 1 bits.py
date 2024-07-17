fce1cecec5b2b078943405667799a87cf445df63
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-17 22:20:00 +0600

    better code: # loop is better than converting to str

--- 2. number of 1 bits.py
+++ 2. number of 1 bits.py
@@ -9,3 +9,12 @@ class Solution:
 #%%
 n=128
 bin(n).count("1")
+# %%
+# loop is better than converting to str
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        c = 0
+        while n:
+            c +=(n&1)
+            n = (n>>1)
+        return c
