2da7f4c1424726c3d41a0581113e9230b1fc32b8
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-17 22:33:32 +0600

    optimazation

--- 3. counting bits.py
+++ 3. counting bits.py
@@ -10,3 +10,13 @@ class Solution:
 n = 5
 [0]+[format(i, "b").count("1") for i in range(1, n+1)]
 # %%
+# don't know how it works
+# if even right shifted number
+# if odd right shifted + 1
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
+                
