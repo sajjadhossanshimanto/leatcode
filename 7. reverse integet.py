60ac8ce1b0909c4f9570b5f7ca3d17afe556e335
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-18 11:26:49 +0600

    corrected with beats 78

--- 7. reverse integet.py
+++ 7. reverse integet.py
@@ -4,12 +4,14 @@ https://leetcode.com/problems/reverse-integer/description/
 #%%
 class Solution:
     def reverse(self, x: int) -> int:
+        if x<0:
+            x = -int(str(-x)[::-1])
+        else:
+            x = int(str(x)[::-1])
+        
         if 2147483647<x or x<-2147483647:
             return 0
-        
-        if x<0:
-            return -int(str(-x)[::-1])
-        return int(str(x)[::-1])
+        return x
 
 #%%
 n=-120
