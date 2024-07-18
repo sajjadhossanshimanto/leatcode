e43739f930cf3ddc95f074f69941fa3b7fd02982
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-18 11:14:02 +0600

    better set approach

--- 5. missing number.py
+++ 5. missing number.py
@@ -11,3 +11,15 @@ class Solution:
         for i in range(len(nums)+1):
             if i not in nums:
                 return i
+#%%
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums= set(nums)
+        arr = set(range(len(nums)+1))
+        return list(arr.difference(nums))[0]
+
+# set is not substractable
+# set pop is slow
+# list()[0] even faster
