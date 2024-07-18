979fe282d1c1c50cc63587b7dfec67ea4366511e
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-18 12:38:43 +0600

    best solution is with two pointer

--- 7.link list cycle.py
+++ 7.link list cycle.py
@@ -47,3 +47,13 @@ for i in l:
 # %%
 s.hasCycle(head)
 # %%
+# two pointer approach
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        while(fast and fast.next):
+            head = head.next
+            fast = fast.next.next
+            if head == fast:
+                return True          
+        return False
