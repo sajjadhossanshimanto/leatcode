0ba9b126d34e8f1b95ca45cea58350682c29dbff
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-18 13:34:52 +0600

    initial code

---
+++ 3. record list.py
@@ -0,0 +1,36 @@
+'''
+https://leetcode.com/problems/reorder-list/description/
+'''
+#%%
+from collections import deque
+from typing import Optional
+from itertools import cycle
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        all_nodes = deque()
+        while head:
+            all_nodes.append(head)
+            head = head.next
+        
+        head = all_nodes.popleft()
+        node = head
+        ope = cycle([all_nodes.pop, all_nodes.popleft])
+        while all_nodes:
+            node.next = next(ope)()
+            node = node.next
+
+        node.next = None
+        return head
+
+#%%
