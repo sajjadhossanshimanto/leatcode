ed9a33e8bfdd4608f92300d8fd2ddb378d5114ff
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-18 11:08:43 +0600

    initial ac but beats 29

---
+++ 5. missing number.py
@@ -0,0 +1,13 @@
+'''
+https://leetcode.com/problems/missing-number/description/
+'''
+#%%
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums= set(nums)
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
