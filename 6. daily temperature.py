a2c4ef8693c71a0bb26edd43a2fed8c8495a0e6f
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-21 14:04:52 +0600

    better approach with min heap

--- 6. daily temperature.py
+++ 6. daily temperature.py
@@ -15,3 +15,18 @@ class Solution:
                     ans[i] = j-i+1
         
         return ans
+
+#%%
+from heapq import heappush, heappop
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0]*len(temperatures)
+        sol = []# list of tuple(num, idx)
+        for pos, temp in enumerate(temperatures):
+            if sol:
+                while temp>=sol[0][0]:# TODO: is't it always the 1st element
+                    min_tem, min_pos = heappop(sol)
+                    ans[min_pos] = min_pos-pos+1# TODO: +! may not rewquired
+
+            heappush(sol, (temp, pos))
