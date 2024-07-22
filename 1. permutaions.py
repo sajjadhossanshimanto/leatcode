214141be5eab7b88f60298003bb89d3a3457f131
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-22 16:15:01 +0600

    Q1 ack track

---
+++ 1. permutaions.py
@@ -0,0 +1,13 @@
+#%%
+from typing import List
+from itertools import permutations
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(permutations(nums))
+
+s= Solution()
+#%%
+s.permute([1, 2, 3])
+# %%
