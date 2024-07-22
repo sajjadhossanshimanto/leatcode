f9bbe69aa107d8c75ffef1edf41fa8b6f9b05599
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-22 16:33:25 +0600

    permutation from lec

--- 1. permutaions.py
+++ 1. permutaions.py
@@ -11,3 +11,20 @@ s= Solution()
 #%%
 s.permute([1, 2, 3])
 # %%
+# %%
+def permutations(nums):
+    if len(nums)==1:
+        return [[nums[0]]]# coping element
+    
+    result = []
+    for i in range(len(nums)):
+        n = nums.pop(0)
+        perms = permutations(nums)
+
+        for perm in perms:
+            perm.append(n)
+        result.extend(perms)
+        nums.append(n)
+    
+    return result    
+
