3782872a5bebaa17f7c882fdb1a1d5ba26e96eea
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-22 13:29:02 +0600

    failed backwork checking

--- 16. II remove all adjacent duplicats in str.py
+++ 16. II remove all adjacent duplicats in str.py
@@ -6,44 +6,18 @@ from typing import List
 inf = float('inf')
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> int:
-        parent = [-1]*len(s)
-        def find(a: int):
-            # if parent[a]==-1:
-            #     return 
-            if parent[a]<0: return a
-
-            r = find(parent[a])
-            parent[a] = r
-            return r
-
-        remove = []
-        def union(a:int, b:int):
-            ''' make a as parent unconditionally '''
-            p1 = find(a)
-            p2 = find(b)
-            parent[p1] += parent[p2]
-            parent[p2] = p1
-            # union node count has reached the limit
-            # remove.append((p1, b))# from b to the start
-            # TODO how am i gona remove it
-
-        ans = s
-        i = 1
+        i = k-1# zero index
         while i!=len(s):
-            if s[i]==s[i-1]:
-                union(i-1, i)
-                # as after this 
-                p1 = find(i-1)
-                if abs(parent[p1])==k:
-                # remove from p1 to i/ can be said as p1+i
-                    s = "".join([s[:p1], s[p1+k+1:]])
-                    for j in range(p1, p1+k):
-                        parent[j] = -1
-                        parent.pop()
-                    i=p1#TODO: should i -1 to p1
-            else:
-                i+=1
-        
+            char = s[i]
+            for j in range(i-1, i-k, -1):
+                if s[j]!=char:
+                    i +=1
+                    break
+            else:# if not breaked
+                s = "".join([s[:i-k+1], s[i+1:]])
+                # back steping
+                # while 
+            # return
         return s
 
 s = Solution()
