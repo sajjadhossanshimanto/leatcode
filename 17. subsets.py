dc431a297980873e89f1b1d29234ab99228d2e70
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-23 18:18:05 +0600

    previous solutions includes [1, 2] but not [1, 3]

--- 17. subsets.py
+++ 17. subsets.py
@@ -7,9 +7,8 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = [[]]
         for ln in range(1, len(nums)):
-            for i in range(0, len(nums)-ln+1):
-                ans.append(nums[i:i+ln])
-                # print([i, i+ln])
+            ans.extend(combinations(nums, r=ln))
+
         ans.append(nums)
         return ans
 
