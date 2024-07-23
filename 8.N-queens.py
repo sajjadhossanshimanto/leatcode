241c0f0c51402cfb4699613a035fe068852235c5
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-23 13:17:14 +0600

    set and unset on same function

--- 8.N-queens.py
+++ 8.N-queens.py
@@ -11,27 +11,28 @@ class Solution:
         def is_valid(x, y):
             pass
 
-        def place_queen(x, y):
+        def place_queen(x, y, place=1):
             # mark row
-            visit[0] = [1]*n
+            visit[0] = [place]*n
 
             # mark col
             for i in range(n):
-                visit[i][y] = 1
+                visit[i][y] = place
 
             # mark right-corner
             for i, j in zip(range(x+1, n), range(y+1, n)):
-                visit[i][j] = 1
+                visit[i][j] = place
 
             # mark left-corner
             for i, j in zip(range(x-1, -1, -1), range(y-1, -1, -1)):
                 if i<0 or j<0: break
 
-                visit[i][j] = 1
+                visit[i][j] = place
 
         for x in range(n):
             for y in range(n):
                 if not visit[x][y]:
-                    place_queen(x, y)
+                    place_queen(x, y, place=1)
+                    break
 
 # %%
