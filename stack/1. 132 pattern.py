9a13caacba9bdab71b051a3f70a26fb136efc30c
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-26 17:31:20 +0600

    corrected according right question statement

--- stack/1. 132 pattern.py
+++ stack/1. 132 pattern.py
@@ -1,12 +1,26 @@
-#  see web
+'''
+https://leetcode.com/problems/132-pattern/description/
+'''
 #%%
-class solution:
-    def find132pattern(self, num):
-        index = {}
-        seq = [231]
-        for pos, i in enumerate(num):
-            if seq and i==seq[-1]:
-                index[i]=pos
-                seq.pop()
-        
-        return index[1]<index[3]<index[2]
+from typing import List
+from itertools import combinations
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        for i, j, k in combinations(nums, r = 3):
+            if i<k<j:
+                return True
+        return False
+
+
+s = Solution()
+# %%
+# ans = false
+s.find132pattern([1,2,3,4])
+# %%
+# ans = 1
+s.find132pattern([3,1,4,2])
+# %%
+# 1
+s.find132pattern([-1,3,2,0])
+# %%
