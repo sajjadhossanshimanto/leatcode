3c8d12a62abe667a4231d16b4039bc79f223c795
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-27 15:04:24 +0600

    thiking the proablem in wrong way. corrected with ac

--- stack/7. min stack.py
+++ stack/7. min stack.py
@@ -1,24 +1,46 @@
-
-# see web
+'''
+https://leetcode.com/problems/min-stack/description/
+'''
 #%%
 from typing import List
 
 
+inf = float('inf')
 class MinStack:
     def __init__(self):
         self.stack = []
-        self.min = 0# TODO: what should be the default 
+        self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if val<self.min:
-            self.min = val
+        self.min_stack.append(
+            min(val, self.min_stack[-1]) if self.min_stack else val
+        )
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.min
+        if self.stack:
+            return self.min_stack[-1]
+# %%
+a = MinStack()
+a.push(46)
+a.push(46)
+a.push(47)
+a.pop()
+print(a.getMin())
+a.pop()
+print(a.getMin())
+a.pop()
+a.push(47)
+print(a.getMin())
+a.push(-48)
+print(a.getMin())
+a.pop()
+print(a.getMin())
+# %%
