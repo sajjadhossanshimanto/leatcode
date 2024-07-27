ca3595488e6ab9543004e43f7cf606db3753e2ae
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-27 15:49:44 +0600

    var rename

--- stack/11. largest rectangle in histogram.py
+++ stack/11. largest rectangle in histogram.py
@@ -4,7 +4,7 @@ from typing import List
 
 inf = float('inf')
 class Solution:
-    def largestRectngleArea(self, heights: List[int]) -> int:
+    def largestRectangleArea(self, heights: List[int]) -> int:
         # heights = sorted(heights)# TODO: iterable sorter. i think its not possible until loop over all the ilement can't determin confirmly whitch one the max
         heights.sort()
         # TODO: list sort vs sorted time
@@ -20,7 +20,7 @@ class Solution:
 
 s = Solution()
 # %%
-s.largestRectngleArea([2, 1, 5, 6, 2, 3])
+s.largestRectangleArea([2, 1, 5, 6, 2, 3])
 # %% random
-s.largestRectngleArea([2, 1, 2, 3, 2])
+s.largestRectangleArea([2, 1, 2, 3, 2])
 # %%
