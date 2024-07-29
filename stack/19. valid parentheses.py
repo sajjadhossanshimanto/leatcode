f9f034e4a126860491e2275995ef5bf732472a52
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-07-29 18:40:38 +0600

    some edge case

--- stack/19. valid parentheses.py
+++ stack/19. valid parentheses.py
@@ -14,11 +14,14 @@ class Solution:
         opened = []
         for i in s:
             if i in close_pair:
+                if not opened:# safety for pop
+                    return False
                 if opened.pop() != close_pair[i]:
                     return False
             else:
                 opened.append(i)
-        
+         
+        if opened: return False
         return True
 
 s= Solution()
