2019223afb292e354aa7eff2c54ee7fa2519ba6f
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-01 14:09:03 +0600

    solution reveals falty test cases. my code gets ac

--- stack/15.5. I remove all adjacent duplicats in str.py
+++ stack/15.5. I remove all adjacent duplicats in str.py
@@ -11,18 +11,43 @@ class Solution:
             else:
                 counter.append([char, 1])
 
-            if counter[-1][1]>=2:
+            if counter[-1][1]>1:
                 counter.pop()
 
         return "".join(i for i, _ in counter)
 
 s = Solution()
+#%%
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        # Create a stack to store characters.
+        st = []
+
+        # Iterate through each character of the input string.
+        for char in s:
+            # Check if the stack is not empty.
+            if st:
+                # If the current character is equal to the top of the stack, remove the duplicate.
+                if st[-1] == char:
+                    st.pop()
+                else:
+                    # Push the current character onto the stack.
+                    st.append(char)
+            else:
+                # If the stack is empty, push the current character onto the stack.
+                st.append(char)
+
+        # Build the result string by joining characters from the stack.
+        result = ''.join(st)
+
+        return result
+
+s = Solution()
+s.removeDuplicates(s = "aeeed")
 # %%
 # ans = "abcd"
 s.removeDuplicates(s = "abcd")
 # %%
-# ans = ""
-s.removeDuplicates(s = "deeedbbcccbdaa")
 # %%
 # ans = "ca"
 s.removeDuplicates(s = "abbaca")
