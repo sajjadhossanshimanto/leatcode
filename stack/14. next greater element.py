1330c0d0d9f9892b599aec5310ca1adc50892c2c
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-01 20:18:24 +0600

    test cases

--- stack/14. next greater element.py
+++ stack/14. next greater element.py
@@ -16,15 +16,11 @@ class Solution:
             index[num] = pos
         
         ans = [-1]*len(nums1)
-        stack = []# of pos
-        # while nums2:
-        #     num = nums2.pop()
-        # for num in (reversed(nums2)):
-        # for pos in range(len(nums2)-1, -1, -1):
-            # num = nums2[pos]
-        for pos, num in reversed(nums2):
-            if num not in index: continue
-
-            while stack and num > nums2[stack[-1]]:
-                pre_idx = stack.pop()
-                ans[pre_idx] = idx - pre_idx
+s = Solution()
+# %%
+# ans = [-1, 3, -1]
+s.nextGreaterElement(nums1 = [4,1,2], nums2 = [1,3,4,2])
+# %%
+# ans = [3, -1]
+s.nextGreaterElement(nums1 = [2,4], nums2 = [1,2,3,4])
+# %%
