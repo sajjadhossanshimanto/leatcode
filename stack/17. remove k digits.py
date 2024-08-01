ad759903cda8dac3ff5f6f492073cf446bd930dd
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-01 16:29:45 +0600

    beats 80% watched from solutionn but optimised

--- stack/17. remove k digits.py
+++ stack/17. remove k digits.py
@@ -11,17 +11,20 @@ class Solution:
         if k==len(num): return "0"
 
         n = len(num) - k
+        ans = 0
         stack = []
         for i in num:
-            while stack and i>stack[-1]: 
+            while stack and i<stack[-1] and k>0:# we can at max remove k. any more removal would rest error
                 # build stack prefering smaller number
                 stack.pop()
-                
+                k-=1
             stack.append(i)
 
-        #TODO: what if removal reduces stack ln tobe less than `n`
-        res = ''.join(islice(stack, len(stack)-n, None))
-        return res or "0"
+        ans = ''.join(
+            islice(stack, len(stack)-k) if k else stack
+        ).lstrip("0")
+
+        return ans or "0"
 
 s = Solution()
 # %%
@@ -30,4 +33,6 @@ s.removeKdigits('1432219', k=3)
 s.removeKdigits("10200", k=1)
 # %%
 s.removeKdigits("10", 2)
+#%%
+s.removeKdigits("112", 1)
 # %%
