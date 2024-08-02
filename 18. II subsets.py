346964de8a1f8f915d5e33707eba9ec8270fbc82
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-02 17:42:13 +0600

    solution from ans

--- 18. II subsets.py
+++ 18. II subsets.py
@@ -13,6 +13,22 @@ class Solution:
         ans.add(nums)
         return ans
 
+#%%
+# from solution
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        # memorising the path we are going through
+        def backtrack(i, path):
+            res.append(path)
+            for j in range(i, len(nums)):
+                # as nums sorted all duplicates are adjecent
+                if j > i and nums[j] == nums[j-1]: continue
+                backtrack(j+1, path+[nums[j]])
+        backtrack(0, [])
+        return res
+
 # %%
 Solution().subsetsWithDup([1, 2, 3])
 # %%
