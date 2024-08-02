bbe2c9b9fe1546b576f49b4f6a645819d8c700c7
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-02 18:08:18 +0600

    my equvalet solution

--- 13. II Permutations.py
+++ 13. II Permutations.py
@@ -20,3 +20,23 @@ s.permuteUnique([1, 2, 3])
 # [[1,1,2], [1,2,1], [2,1,1]]
 s.permuteUnique([1, 2, 1])
 # %%
+# my equvalet solution
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def helper(i):
+            if i == len(nums) - 1:
+                res.append(nums[:])
+                return
+            
+            hash = {}
+            for j in range(i, len(nums)):
+                if nums[j] not in hash:
+                    hash[nums[j]] = True
+                    nums[i], nums[j] = nums[j], nums[i]
+                    helper(i+1)
+                    nums[i], nums[j] = nums[j], nums[i]
+        helper(0)
+        return res
+
