3a94b7d210e16debf7e5b278aeff599b5f2261d7
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-02 15:14:44 +0600

    forced ans with itertools

--- 18. II subsets.py
+++ 18. II subsets.py
@@ -8,7 +8,14 @@ class Solution:
         nums = tuple(nums)
         ans = {tuple()}
         for ln in range(1, len(nums)):
-            ans.update(combinations(nums, r=ln))
+            ans.update(
+                map(
+                    tuple, # list not hashible
+                    map(# hash varies depending element pos (1, 4) & (4, 1) is diff
+                        sorted, combinations(nums, r=ln)
+                    )
+                )
+            )
 
         ans.add(nums)
         return ans
@@ -19,4 +26,8 @@ Solution().subsetsWithDup([1, 2, 3])
 Solution().subsetsWithDup([1, 2, 2])
 # %%
 Solution().subsetsWithDup([0])
+# %% wa 15
+# ans = [[],[1],[1,4],[1,4,4],[1,4,4,4],[1,4,4,4,4],[4],[4,4],[4,4,4],[4,4,4,4]]
+# out = [[],[1],[4,4],[4,4,1,4],[4,4,4,4],[4,1,4],[4,4,1],[4],[1,4],[4,4,4],[4,4,4,1,4],[4,1],[4,4,4,1]]
+Solution().subsetsWithDup([4,4,4,1,4])
 # %%
