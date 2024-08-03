3901f109632a8f09b7a5c5f6d9d99f96723f7057
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-03 14:49:29 +0600

    removing function call beats 81% improves 8ms

--- 10. II N-queens.py
+++ 10. II N-queens.py
@@ -11,38 +11,25 @@ class Solution:
         left_corner = set()# if used arr need n+n space
         right_corner = set()
 
-        def place_queen(x, y):
-            # mark col
-            visit_col.add(y)
-
-            # mark right-corner
-            right_corner.add(x-y)
-
-            # mark left-corner
-            left_corner.add(x+y)
-
-        def remove_queen(x, y):
-            # mark col
-            visit_col.remove(y)
-
-            # mark right-corner
-            right_corner.remove(x-y)
-
-            # mark left-corner
-            left_corner.remove(x+y)
-
         count = [0]
         def dfs_row(x):
             for y in range(n):
                 if y in visit_col or (x+y) in left_corner or (x-y) in right_corner : continue
 
-                place_queen(x, y)                
+                # place_queen(x, y)
+                visit_col.add(y)
+                right_corner.add(x-y)
+                left_corner.add(x+y)
+
                 if x<n-1:# next row exists
                     dfs_row(x+1)
                 if x==n-1:
                     count[0]+=1
 
-                remove_queen(x, y)
+                # remove queen
+                visit_col.remove(y)
+                right_corner.remove(x-y)
+                left_corner.remove(x+y)
 
         dfs_row(0)
         return count[0]
