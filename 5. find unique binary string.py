1b0f5f5f12749f6c381b737e67f4c5196ed75990
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-03 15:07:53 +0600

    ac beats 44

--- 5. find unique binary string.py
+++ 5. find unique binary string.py
@@ -1,13 +1,15 @@
+'''
+https://leetcode.com/problems/find-unique-binary-string/description/
+'''
 #%%
 from typing import List
-from itertools import combinations
 
 
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         n = len(nums[0])
         nums = set(nums)
-        for i in range(int("1"+"0"*n, base=2)):
+        for i in range(int(f"1{'0'*n}", base=2)):
             b = format(i, "b")
             b = "0"*(n-len(b)) + b
             if b not in nums:
