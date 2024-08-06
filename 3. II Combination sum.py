59f6300faf7255aa7da89e90f1906962b7c95c3b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-06 17:34:24 +0600

    test case added

--- 3. II Combination sum.py
+++ 3. II Combination sum.py
@@ -20,7 +20,9 @@ class Solution:
 
 s = Solution()
 # %%
-s.combinationSum([2, 3, 6, 7], 7)
+# [ [1,1,6], [1,2,5], [1,7], [2,6] ]
+s.combinationSum(candidates = [10,1,2,7,6,1,5], target = 8)
 # %%
-s.combinationSum([10, 1, 2, 7, 6, 1, 5], 8)
-# %%
+# [ [1,2,2], [5] ]
+s.combinationSum(candidates = [2,5,2,1,2], target = 5)
+#%%
