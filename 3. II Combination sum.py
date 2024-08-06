c49736f2f052ed6f1d844779d73a224d96e3aa26
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-06 18:26:25 +0600

    code from sol 1

--- 3. II Combination sum.py
+++ 3. II Combination sum.py
@@ -1,28 +1,32 @@
 #%%
 from typing import List
-from itertools import combinations_with_replacement
-
 
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        filter(lambda x:x>target, candidates)
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
 
-        res = []
-        for i in range(1, len(candidates)):
-            res.extend(
-                filter(
-                    lambda x:sum(x)==target,# TODO generate sum on the go
-                    combinations_with_replacement(candidates, r=i)
-                )
-            )
+        res = set()
+        def backtrack(start: int, pre_sum: int, path: list) -> list[int, list]:
+            if start>=len(candidates): return
+            if pre_sum>=target: return
 
+            for pos in range(start, len(candidates)):
+                total = pre_sum + candidates[pos]
+                path.append(candidates[pos])
+                if total<target:
+                    backtrack(pos, total, path)
+                elif total==target:
+                    res.add(tuple(path))
+                path.pop()
+
+        backtrack(0, 0, [])
         return res
 
 s = Solution()
 # %%
 # [ [1,1,6], [1,2,5], [1,7], [2,6] ]
-s.combinationSum(candidates = [10,1,2,7,6,1,5], target = 8)
+s.combinationSum2(candidates = [10,1,2,7,6,1,5], target = 8)
 # %%
 # [ [1,2,2], [5] ]
-s.combinationSum(candidates = [2,5,2,1,2], target = 5)
+s.combinationSum2(candidates = [2,5,2,1,2], target = 5)
 #%%
