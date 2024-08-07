3b3048989e6d94fe08a1453cb3e5b5c9879ed142
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-07 15:42:20 +0600

    usuing combination sum. not works actually. just making target sum is not enough we have to distribute nums egually

--- 7. Matchstricks to square.py
+++ 7. Matchstricks to square.py
@@ -9,34 +9,42 @@ class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
         if len(matchsticks)<4: return False
 
-        side, remin = divmod(sum(matchsticks), 4)# O(n)
+        target, remin = divmod(sum(matchsticks), 4)# O(n)
         if remin: return False
 
         matchsticks.sort()# O(n)
         # error: we can't bend any stick
-        if matchsticks[-1]>side: return False
+        if matchsticks[-1]>target: return False
 
-        sides = [side]*4
-        while matchsticks:
-            stick = matchsticks.pop()
+        res = [0]
+        visit = [0]*len(matchsticks)
+        def backtrack(start: int, pre_sum: int) -> list[int, list]:
+            if start>=len(matchsticks): return # base case: end of tree
 
-            # error: there are stick left but no place
-            if not sides: return False
-            
-            # find a proper pos to place stick
-            pos = len(sides) - 1
-            while pos>=0:
-                if sides[pos] >= stick:
-                    break
-                pos -=1
-            else:
-                # error: no place found to place
-                return False
-            
-            sides[pos] -= stick
-            if sides[pos]==0: sides.pop(pos)
-        
-        return True
+            for pos in range(start, len(matchsticks)):
+                i = matchsticks[pos]
+                if visit[pos]: continue
+
+                total = pre_sum + i
+                visit[pos] = 1
+                if total<target:
+                    preserve = backtrack(pos+1, total)
+                elif total>target:
+                    preserve = False
+                elif total==target:
+                    # TODO: ensure donot remove from visit
+                    res[0] += 1
+                    preserve = True
+                
+                if preserve:
+                    if pre_sum!=0:# preserve root
+                        return True
+                else:
+                    visit[pos] = 0
+
+        backtrack(0, 0)
+
+        return res[0]==4
 
 s = Solution()
 # %%
@@ -45,4 +53,10 @@ s.makesquare([1,1,2,2,2])
 # %%
 # ans = 0
 s.makesquare([3,3,3,3,4])
+# %% wa176
+# ans = 1
+# out = 0
+s.makesquare([5,5,5,5,4,4,4,4,3,3,3,3])
+# distribution should be -> 5+4+3 on each side
+# but mine substrack all larges 
 # %%
