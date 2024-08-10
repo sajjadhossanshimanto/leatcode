e07ea8caaffc03f2807e555cface2b547c1a26b8
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-10 18:08:19 +0600

    counter matching gets ac 85% beat

--- 8.permutation is string.py
+++ 8.permutation is string.py
@@ -1,26 +1,38 @@
 '''
 https://leetcode.com/problems/permutation-in-string/description/
+- need O(n) algo
+- as inpuut 10^4
 '''
 #%%
 from typing import List
-from itertools import permutations
 
-#%%
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         # s1 is pattern
         if len(s1) > len(s2): return False
 
-        set1 = set()
+        window = {}
+        pattern = {}
+        for i in range(len(s1)):
+            pattern[s1[i]] = pattern.get(s1[i], 0) + 1
+            window[s2[i]] = window.get(s2[i], 0) + 1
+
+        if pattern==window: return True
+
         l = 0
-        r = len(s1)
-        while r<=len(s2):
-            set1.add(s2[l:r])
+        for r in range(len(s1), len(s2)):
+            # add
+            window[s2[r]] = window.get(s2[r], 0) + 1
+
+            # remove
+            window[s2[l]] -= 1
+            if window[s2[l]]==0:
+                window.pop(s2[l])
             l+=1
-            r+=1
         
-        for i in permutations(s1):
-            if "".join(i) in set1: return True
+            # check
+            if pattern==window: return True
         
         return False
 
