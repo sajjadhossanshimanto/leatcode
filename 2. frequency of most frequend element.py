55815e30dc91872d18520b5f0ec09cfd2e53dba6
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-11 19:26:28 +0600

    farnished the code mathemathically

--- 2. frequency of most frequend element.py
+++ 2. frequency of most frequend element.py
@@ -1,37 +1,37 @@
 '''
 https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/
+- nums length 10e5
+- k = 10e5
+- so need a O(n)
 '''
 #%%
 from typing import List
 
 
+# who said the sum has to be from start to i
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         max_count = 0
         ans = None
-        pre_sum = []
-        counter = {}
-        for i in range(len(nums)):
-            if i>0:
-                pre_sum.append(pre_sum[i-1] + nums[i])
-            else:
-                pre_sum.append(nums[i])
-            counter[i] = counter.get(i, 0) + 1
-            
-            # at most k
-            for j in range(k):
-                div, mod = divmod((pre_sum[-1]+j), (i+1))
-                if mod == 0:
-                    #TODO: should i include if div not in counter
-                    counter[div] = counter.get(div, 0) + (i+1)
-                    if counter[div]>max_count:
-                        ans = div
-                        max_count = counter[div]
+        pre_sum = [k]
         
-        return ans
+        for i in range(1, len(nums)+1):
+            # i is 1 indexed. 
+            # It is the devidor - index - count
+            pre_sum.append(pre_sum[i-1] + nums[i-1])
+
+            # at most k
+            num = int(pre_sum[-1]/i)
+
+        return num
 
 s = Solution()
 # %%
-# 4
+# 3
 s.maxFrequency([1, 2,  4], 5)
 # %%
+# 2
+s.maxFrequency(nums = [1,4,8,13], k = 5)
+# %%
+# 1
+s.maxFrequency(nums = [3,9,6], k = 2)
