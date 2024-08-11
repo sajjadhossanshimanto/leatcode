2ef49f6c3aa871b8a22ef71e379b8c9400d96def
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-11 17:52:49 +0600

    i was missing keyword `greater or equal`. fixxed code gets ac beats 61

--- 6. minimum  size subarray sum.py
+++ 6. minimum  size subarray sum.py
@@ -1,5 +1,6 @@
 '''
 https://leetcode.com/problems/minimum-size-subarray-sum/description/
+- sub-array whome sum is greater or equal
 '''
 #%%
 from typing import List
@@ -14,12 +15,10 @@ class Solution:
         for r in range(len(nums)):
             total += nums[r]
             # if total > target:
-            while total >target:
+            while total >= target:
+                ans = min(ans, r-l+1)
                 total -= nums[l]
                 l+=1
-
-            if total==target:
-                ans = min(ans, r-l+1)
         
         return 0 if ans==inf else ans
 
@@ -33,4 +32,7 @@ s.minSubArrayLen( target = 4, nums = [1,4,4])
 # %%
 # 2
 s.minSubArrayLen(target = 7, nums = [2,3,1,2,4,3])
+# %% wa12
+# 3
+s.minSubArrayLen(11, [1,2,3,4,5])
 # %%
