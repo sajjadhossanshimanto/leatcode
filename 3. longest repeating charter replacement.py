1fccfdb58273f2db95504cd00e134f5b8eaf7269
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-12 15:07:39 +0600

    no need to recrease max counter on the go. blindly remove needed char from left

--- 3. longest repeating charter replacement.py
+++ 3. longest repeating charter replacement.py
@@ -6,19 +6,15 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         window = {}
         max_len = 0
-        max_count = ("$", 0)# (k, v)
+        max_count = 0
         l,r = 0, 0
         for r in range(len(s)):
             window[s[r]] = window.get(s[r], 0) + 1
-            if window[s[r]] > max_count[1]:
-                max_count = [s[r], window[s[r]]]
+            if window[s[r]] > max_count:
+                max_count = window[s[r]]
             
-            # ln_window = r-l+1
-            while (r-l+1) - max_count[1] > k:
-                if s[l] == max_count[0]:
-                    max_count[1] -= 1
+            while (r-l+1) - max_count > k:
                 window[s[l]] -= 1
-                # TODO: do i need to remove if 0
                 l += 1
             
             max_len = max(max_len, r-l+1)
@@ -32,4 +28,6 @@ s.characterReplacement(s = "ABAB", k = 2)
 # %%
 # 4
 s.characterReplacement( s = "AABABBA", k = 1)
+# %% wa41
+s.characterReplacement("BAAA", 0)
 # %%
