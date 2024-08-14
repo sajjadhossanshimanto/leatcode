087ee9ff0275b415e26720151ed692391dcfb8f8
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-14 15:36:29 +0600

    ac with graph tecnique.

--- 4.bi-tree level oder traversal.py
+++ 4.bi-tree level oder traversal.py
@@ -22,20 +22,18 @@ class Solution:
         stack = deque([root])
         pop_count = 0
         while stack:
-            node = stack.popleft()
-            pop_count += 1
-            if pop_count==2:
-                res.append([])
-                pop_count = 0
+            res.append([])
+            for _ in range(len(stack)):
+                node = stack.popleft()
 
-            if node.left:
-                res[-1].append(node.left.val)
-                stack.append(node.left)
-            if node.right:
-                res[-1].append(node.right.val)
-                stack.append(node.right)
-
-        while not res[-1]:
+                if node.left:
+                    res[-1].append(node.left.val)
+                    stack.append(node.left)
+                if node.right:
+                    res[-1].append(node.right.val)
+                    stack.append(node.right)
+        
+        if not res[-1]:
             res.pop()
 
         return res
@@ -45,12 +43,18 @@ s = Solution()
 from tree_helper import etu_to_tree, process_tree, draw_graph
 
 t = [3,9,20,None,None,15,7]
-t = []
+t = etu_to_tree(t)
+
+# [[3],[9,20],[15,7]]
+s.levelOrder(t)
+#%% wa
 t = [1,2,3,4,None,None,5]
 t = etu_to_tree(t)
 
+# [[1],[2,3],[4,5]]
 s.levelOrder(t)
-# %%
+#%%
+
 process_tree(t)
 draw_graph(0)
 # %%
