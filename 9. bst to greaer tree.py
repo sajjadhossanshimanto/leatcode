00d37bfa8c6821924f96c9936c12a0e5078743a4
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-15 11:31:35 +0600

    similar like in-oder but right first

---
+++ 9. bst to greaer tree.py
@@ -0,0 +1,43 @@
+'''
+https://leetcode.com/problems/convert-bst-to-greater-tree/
+'''
+#%%
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        def dfs(node, pre):
+            r = pre
+            if node.right:
+                r = dfs(node.right, pre)
+            node.val+=r
+
+            if node.left:
+                return  dfs(node.left, node.val)            
+            return node.val
+
+        dfs(root, 0)
+        
+        return root
+
+s = Solution()
+#%%
+from tree_helper import etu_to_tree, process_tree, draw_graph
+
+t = [4,1,6,0,2,5,7,None,None,None,3,None,None,None,8]
+t = etu_to_tree(t)
+
+# [30,36,21,36,35,26,15,None,None,None,33,None,None,None,8]
+t = s.convertBST(t)
+process_tree(t)
+# %%
+t = [0, None, 1]
