ab174bcd8614540fe71f71182af1a247567c3e83
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-16 16:40:02 +0600

    ac with 2 process dfs

--- 12. diameter of bi-tree.py
+++ 12. diameter of bi-tree.py
@@ -1,5 +1,6 @@
 '''
 https://leetcode.com/problems/diameter-of-binary-tree/description/
+- it may or nay not involve root
 '''
 #%%
 from typing import List, Optional
@@ -16,23 +17,40 @@ class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         if not root: return 0
 
+        ans = [0]
         def dfs(node):
-            if not node: return 0
+            l, r = 0, 0
+            if node.left: l = dfs(node.left) + 1
+            if node.right: r = dfs(node.right) + 1
 
-            return max(dfs(node.left) + 1, dfs(node.right) + 1)
+            ans[0] = max(ans[0], l+r)
+            return max(l, r)
 
-        return dfs(root.left) + dfs(root.right)
+        dfs(root)
+        return ans[0]
 
 s = Solution()
 # %%
-from tree_helper import etu_to_tree
+from tree_helper import etu_to_tree, process_tree,draw_graph
 
 # # 3
-# t= [1,2,3,4,5]
-
-# # 1
-# t = [1, 2]
-
+t= [1,2,3,4,5]
 t = etu_to_tree(t)
 s.diameterOfBinaryTree(t)
+
+#%%
+# # 1
+t = [1, 2]
+t = etu_to_tree(t)
+s.diameterOfBinaryTree(t)
+
+#%%
+# 8
+t = [4,-7,-3,None,None,-9,-3,9,-7,-4,None,6,None,-6,-6,None,None,0,6,5,None,9,None,None,-1,-4,None,None,None,-2]
+t = etu_to_tree(t)
+# s.diameterOfBinaryTree(t)
+
+process_tree(t)
+# %%
+draw_graph()
 # %%
