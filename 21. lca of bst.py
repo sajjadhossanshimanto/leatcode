33684ca61c147bd1b9f07ad25c88f433f750c6e2
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-08-17 19:57:08 +0600

    lca spacifig for bst

---
+++ 21. lca of bst.py
@@ -0,0 +1,22 @@
+'''
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+'''
+#%%
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        cur = root
+
+        while 1:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            else:
+                return cur
