d1cedfe252c9e582d683eba675d9bde13dfc9554
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-28 21:28:35 +0600

    memory effi: no need of extra list

--- dynamic programming/2. 746. Min Cost Climbing Stairs.py
+++ dynamic programming/2. 746. Min Cost Climbing Stairs.py
@@ -5,14 +5,13 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        min_cost = [cost[0], cost[1]]
         for i in range(2, len(cost)):
-            min_cost.append(min(
-                cost[i]+min_cost[i-1],
-                cost[i]+min_cost[i-2]
+            cost[i] = (min(
+                cost[i]+cost[i-1],
+                cost[i]+cost[i-2]
             ))
 
-        return min(min_cost[-1], min_cost[-2])
+        return min(cost[-1], cost[-2])
         # return min_cost
 #%%
 s = Solution()
