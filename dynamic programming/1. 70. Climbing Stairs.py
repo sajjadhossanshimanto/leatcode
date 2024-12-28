442d7e8f6d731e5e0dd7fafe775af405adab9871
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-28 21:45:17 +0600

    implimentation without extra array

--- dynamic programming/1. 70. Climbing Stairs.py
+++ dynamic programming/1. 70. Climbing Stairs.py
@@ -14,4 +14,16 @@ class Solution:
 #%%
 s = Solution()
 s.climbStairs(2)
-# %%
+# %% -> don't memorise
+# we dont need the entire array just need the last index
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+        if n==1: return 1
+
+        for i in range(2, n+1):
+            fib = a+b
+            a = b
+            b = fib
+        
+        return fib
