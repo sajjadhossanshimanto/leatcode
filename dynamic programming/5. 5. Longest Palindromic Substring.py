b0b9fad80148fa2b04d4e4c0815c3af9f43a3de4
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-29 10:28:20 +0600

    ac: even palindrom

--- dynamic programming/5. 5. Longest Palindromic Substring.py
+++ dynamic programming/5. 5. Longest Palindromic Substring.py
@@ -18,7 +18,18 @@ class Solution:
                 
                 l-=1
                 r+=1
-        
+
+            # even
+            l, r = i, i+1
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                ln = r-l+1
+                if ln>mxln:
+                    mxpal = s[l:r+1]
+                    mxln = ln
+                
+                l-=1
+                r+=1
+
         return mxpal
 
 #%%
