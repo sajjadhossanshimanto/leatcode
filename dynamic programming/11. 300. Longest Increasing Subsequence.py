ce42376280dbbd16325da9ff4ff127eb739ef2b9
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-29 23:00:03 +0600

    my implimentation with decition tree

--- dynamic programming/11. 300. Longest Increasing Subsequence.py
+++ dynamic programming/11. 300. Longest Increasing Subsequence.py
@@ -2,18 +2,31 @@
 
 #%%
 from typing import List
+from functools import lru_cache
+
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        stack = []
-        for i in nums:
-            while stack and i <= stack[-1]:
-                stack.pop()
-            stack.append(i)
-        return len(stack)
+        max_ln = [0]
+        @lru_cache
+        def dfs(start):
+            if start==len(nums)-1:
+                return 1
+            
+            r=1
+            for i in range(start+1, len(nums)):
+                if nums[i]>nums[start]:
+                    r = dfs(i)+1
+                    if r>max_ln[0]: max_ln[0] = r
+            return r
 
-# %%
+        for i in range(len(nums)):
+            dfs(i)
+        
+        return max_ln[0]
+
 s = Solution()
+# %%
 s.lengthOfLIS(
     [7,7,7,7,7,7,7]
 )
@@ -21,5 +34,13 @@ s.lengthOfLIS(
 # ans: 4
 s.lengthOfLIS(
     [0,1,0,3,2,3]
+)
+# %%
+s.lengthOfLIS(
+    [10,9,2,5,3,7,101,18]
+)
+# %%
+s.lengthOfLIS(
+    [1, 2, 3, 4, 5]
 )
 # %%
