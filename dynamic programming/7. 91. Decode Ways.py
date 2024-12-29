83fbadc7ad942d3ddfae15e9075f0a190a285e8e
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-29 11:39:09 +0600

    approach 3: recursion. slight edge case exists

--- dynamic programming/7. 91. Decode Ways.py
+++ dynamic programming/7. 91. Decode Ways.py
@@ -6,15 +6,32 @@ from itertools import permutations
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        return len(s)*(len(s)-1)
-        count = len(set(s))
-        ways = set()
-        for i in range(len(s)-1):
-            ways.add(s[i]+s[i+1])
-        
-        return count*len(ways)
+        count = [0]
+        def dfs(start):
+            if start==len(s):
+                count[0]+=1
+                return 
+            
+            # take one
+            dfs(start+1)
+
+            # take 2
+            if start<len(s)-1:
+                if s[start: start+2] <= "26":
+                    dfs(start+2)
+            
+        dfs(0)
+        return count[0]
 
 # %%
 s = Solution()
+# ans: 3
 s.numDecodings("226")
+# %%
+# ans: 2
+s.numDecodings("12")
+# %%
+# ans = 0
+# out = 2
+s.numDecodings("06")
 # %%
