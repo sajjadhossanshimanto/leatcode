c9041fe1412b10d90f431eb7dc618ad6e5be0a16
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-30 11:25:00 +0600

    file rename

--- dynamic programming/9. 152. Maximum Product Subarray.py
+++ dynamic programming/9. 152. Maximum Product Subarray.py
@@ -1,25 +1,31 @@
 # https://leetcode.com/problems/maximum-product-subarray/description/
 
 #%%
-from itertools import combinations
 from typing import List
-
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_mul = 0
-        for i in range(1, len(nums)):
-            for j in combinations(nums, i):
-                mul = 1
-                for k in j: mul*=k
-                if mul>max_mul: max_mul = mul
-        
-        return max_mul
+        suffix = 1
+        prefix = 1
+        max_ = max(nums)
+        n = len(nums)
+        for i in range(n):
+            if suffix==0: suffix = 1
+            if prefix==0: prefix = 1
 
+            prefix *= nums[i]
+            suffix *= nums[n-i-1]
+
+            max_ = max(prefix, suffix, max_)
+
+        return  max_
 # %%
-# ans: 6
 s = Solution()
 s.maxProduct(
     [2,3,-2,4]
+)
+# %%
+s.maxProduct(
+    [-2,0,-1]
 )
 # %%
