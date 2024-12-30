c44c0581eb0af56a3e38e816457057a2a5ff2c38
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-30 19:36:06 +0600

    dp solution

--- dynamic programming/13. 62. Unique Paths.py
+++ dynamic programming/13. 62. Unique Paths.py
@@ -18,6 +18,21 @@ class Solution:
 
         return dfs(0, 0)
 
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * n for _ in range(m)]
+        
+        # loop will iterate through doted lines
+        # 1111
+        # 1---
+        # 1---
+        for i in range(1, m):# note: zero is skipped
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        # The bottom-right corner contains the result
+        return dp[m - 1][n - 1]
+
 s = Solution()
 # %%
 s.uniquePaths(m = 3, n = 7)
