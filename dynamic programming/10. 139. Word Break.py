9c0b6b3e0e014f99ad303a87819a1f2458427c15
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-30 12:17:29 +0600

    dp solution from needcode. decition tree performs better

--- dynamic programming/10. 139. Word Break.py
+++ dynamic programming/10. 139. Word Break.py
@@ -18,6 +18,21 @@ class Solution:
         
         return dfs()
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False]*(len(s)+1)
+        dp[-1] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for word in wordDict:
+                if s[i: i+len(word)]==word:
+                    dp[i] = dp[i+len(word)]
+                    
+                    if dp[i]: break
+        
+        return dp[0]
+
+
 s= Solution()
 # %%
 # ans: true
