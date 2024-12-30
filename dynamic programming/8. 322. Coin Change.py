d00468b1f1c8f078fd8b91cf8fb855725e238248
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-30 13:22:33 +0600

    ac beats 36% only

--- dynamic programming/8. 322. Coin Change.py
+++ dynamic programming/8. 322. Coin Change.py
@@ -2,53 +2,25 @@
 
 #%%
 from typing import List
+from functools import lru_cache
 
 
+inf = float("inf")
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort()
-        count = 0
-        for i in range(len(coins)-1, -1, -1):
-            if amount<coins[i]: continue
-            c, amount=divmod(amount, coins[i])
-            count+=c
+        @lru_cache(None)
+        def dfs(money):
+            if money==0: return 0
+            
+            count = inf
+            for i in coins:
+                if money>=i:
+                    count = min(dfs(money-i)+1, count)
+            
+            return count
         
-        return count if amount==0 else -1
-
-# %%
-s= Solution()
-s.coinChange(
-    coins = [1,2,5], amount = 11
-)
-# %%
-s.coinChange(
-    coins = [2], amount = 3
-)
-# %%
-s.coinChange(
-    coins = [1], amount = 0
-)
-# %% wa -> 408 may devide given ammount better than 419
-s.coinChange(
-    [186,419,83,408],
-    6249
-)
-# %%
-
-#%%
-from typing import List
-
-
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort()
-        count = 0
-        for i in range(len(coins)-1, -1, -1):
-            if amount<coins[i]: continue
-            c, amount=divmod(amount, coins[i])
-            count+=c
-        
-        return count if amount==0 else -1
+        result = dfs(amount)
+        return result if result != inf else -1
 
 # %%
 s= Solution()
