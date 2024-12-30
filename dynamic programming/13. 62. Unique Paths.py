5bec85b7b7bea3fe541d50adeca436f89b1741ff
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-30 19:30:28 +0600

    dfs with new logic. which is actually the equvalent implementation of dp

--- dynamic programming/13. 62. Unique Paths.py
+++ dynamic programming/13. 62. Unique Paths.py
@@ -1,23 +1,22 @@
 # https://leetcode.com/problems/unique-paths/description/
 
 #%%
+from functools import lru_cache
+
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        count = [0]
-        # visit = [[0 for _ in range(n)] for _ in range(m)]
+        
+        @lru_cache(None)
         def dfs(x, y):
             if x==m-1 and y==n-1:
-                count[0]+=1
-                return
-            # if x>=m or y>=n or x<0 or y<0 or visit[x][y]: return
-            if x>=m or y>=n or x<0 or y<0: return
+                return 1
 
-            # visit[x][y] = 1
-            dfs(x+1, y)
-            dfs(x, y+1)
+            if x>=m or y>=n: return 0
 
-        dfs(0, 0)
-        return count[0]
+            return dfs(x+1, y) + dfs(x, y+1)
+
+        return dfs(0, 0)
 
 s = Solution()
 # %%
