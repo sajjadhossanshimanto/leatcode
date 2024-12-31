8ef707f41666c4d34e34a4c5240b6f0dd8680644
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-31 14:59:34 +0600

    basic implimentation of unique path

--- dynamic programming/16.518. Coin Change II.py
+++ dynamic programming/16.518. Coin Change II.py
@@ -8,10 +8,12 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         # path counting proablem
         # but only unique path
-        
-        def dfs(money_left) -> int:# number of ways
-            if money_left==0: 
-                # print(" end point hit")
+        unique_paths = set()
+        cache = {}
+        def dfs(money_left, path=[]) -> int:# number of ways
+            # if money_left in cache: return cache[money_left]
+            if money_left==0:
+                unique_paths.add(tuple(sorted(path)))
                 return 1
             if money_left<0: 
                 return 0
@@ -19,10 +21,15 @@ class Solution:
             count = 0
             for i in coins:
                 # print(i, end=" ")
-                count+=dfs(money_left-i)
+                path.append(i)
+                count+=dfs(money_left-i, path)
+                path.pop()
+            
+            # cache[money_left] = count
             return count
         
-        return dfs(amount)
+        
+        return len(unique_paths)
 
 s = Solution()
 # %%
