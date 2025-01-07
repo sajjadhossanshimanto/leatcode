b8171340324660b475526f7bf98707ec69bd0d80
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-07 21:35:46 +0600

    basic recursion solution

--- dynamic programming/16.518. Coin Change II.py
+++ dynamic programming/16.518. Coin Change II.py
@@ -2,36 +2,40 @@
 
 #%%
 from typing import List
+from functools import lru_cache
 
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # path counting proablem
-        # but only unique path
-        if amount==0: return 1
-        
-        cache = {}
-        # dfs number of inputs represents --> dimention
-        # 2 inputs means 2d
-        # every dfs with caching can be represented as dynamic programming
-        def dfs(money_left, start=0) -> int:# number of ways
-            if (money_left, start) in cache: return cache[(money_left, start)]
-        
-            if start>=len(coins) or money_left<0: return 0
+        coins.sort()
+
+        @lru_cache(None)
+        def dfs(money_left):
             if money_left==0: return 1
 
-            counter = 0
-            for pos in range(start, len(coins)):
-                i = coins[pos]
-                counter+=dfs(money_left-i, pos)
+            count = 0
+            for i in coins:
+                if i>money_left: break
 
-            cache[(money_left, start)] = counter
-            return counter
+                count+=dfs(money_left-i)
+            
+            return count
 
+        return dfs(amount)
 
-        return (dfs(amount))
+s = Solution()
+# %%
+# ans: 5
+# out: 9 as it contain replacement
+s.change(
+    amount = 5, coins = [5, 2, 1]
+)
+# %%
+s.change(
+    amount = 3, coins = [2]
+)
 
-
+#%%
 ## solution by neetcode
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
@@ -48,16 +52,7 @@ class Solution:
             cache[(i, money_left)] = dfs(i, a+coins[i]) + dfs(i+1, a)
 
 s = Solution()
-# %%
-# ans: 5
-# out: 9 as it contain replacement
-s.change(
-    amount = 10, coins = [5, 2, 1]
-)
-# %%
-s.change(
-    amount = 3, coins = [2]
-)
+
 # %%
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
