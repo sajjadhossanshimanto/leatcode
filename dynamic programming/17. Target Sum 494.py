bfc1e51f42fa72023315c212cb985d60ead1192e
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-09 13:50:56 +0600

    edge case 1: position with number zero is also included

--- dynamic programming/17. Target Sum 494.py
+++ dynamic programming/17. Target Sum 494.py
@@ -9,11 +9,12 @@ class Solution:
         # print(target)
 
         def backtrack(start=0, presum=0):
+            count = 0
             if presum==target:
-                return 1
+                count=1
+                # return 1 # edge case 1: there might exists zeros ahead which needs to count
             if presum>target: return 0
 
-            count = 0
             for i in range(start, len(nums)):
                 count += backtrack(i+1, presum+nums[i])
             
@@ -27,3 +28,5 @@ s = Solution()
 # ans = 5
 s.findTargetSumWays(nums = [1,1,1,1,1], target = 3)
 # %%
+# ans: 2
+s.findTargetSumWays([1, 0], 1)
