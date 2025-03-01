c5237faa21ccdc912909234b88e9fcbada96e3e7
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-01 09:28:42 +0600

    1 edge case improved still tle for fractional power

--- Math and geometry/6. pow.py
+++ Math and geometry/6. pow.py
@@ -3,12 +3,23 @@
 leetcode medium. because 
 the power can be negative as well
 
+edge case:
+- power 0 -> is 1
+- TODO: what would be for 0^0??
 '''
 
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        if n==0: return 1# edge case
+
         ans = x
         for _ in range(abs(n)-1):
             ans*=x
         
         return 1/ans if n<0 else ans
+
+s = Solution()
+'''
+gets tle -> as we are not considering fractional power
+'''
+s.myPow(0.00001, 2147483647)
